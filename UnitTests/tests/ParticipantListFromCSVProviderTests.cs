4f2234a982c31baab55001e5930c8a74974587fd
Gal Weiss <gaweiss@microsoft.com>
2019-12-19 23:10:26 +0200

    tests work start

---
+++ UnitTests/tests/ParticipantListFromCSVProviderTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ParticipantListFromCSVProvider_ValidInput_ReturnParticipantList()
+        {
+
+
+        }
+    }
+}
