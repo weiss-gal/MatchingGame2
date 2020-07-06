f0a96688352f68136342a31b5493d5d3e030ad80
gal weiss <weiss_gal@hotmail.com>
2020-07-07 01:47:15 +0300

    added database migration for display name

---
+++ Migrations/20200706224217_add displayname.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MatchingGame2.Migrations
+{
+    public partial class adddisplayname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "AspNetUsers");
+        }
+    }
+}
