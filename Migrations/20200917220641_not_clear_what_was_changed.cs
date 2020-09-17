bba8338f033df7f61121ae2d30c5e17b97e3d816
gal weiss <weiss_gal@hotmail.com>
2020-09-18 01:06:54 +0300

    aligning migrations

---
+++ Migrations/20200917220641_not_clear_what_was_changed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MatchingGame2.Migrations
+{
+    public partial class not_clear_what_was_changed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
